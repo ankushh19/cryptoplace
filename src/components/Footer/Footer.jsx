b1c0c71b808a12a5b2e78f4d0e8d89d56d58e8a8
ankushh19 <ak9108824@gmail.com>
2024-05-28 18:49:42 +0530

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Made with ❤️ by Ankush</p>
+    </div>
+  );
+};
+
+export default Footer;
